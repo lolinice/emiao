d0af2dc04d0a42055c3bc0c3638812b556de5320
lolinice <lolinice99@163.com>
2018-03-19 16:42:53 +0800

    home

--- src/components/FooterGuide/FooterGuide.vue
+++ src/components/FooterGuide/FooterGuide.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="footer_guide">
-    <span href="" class="guide_item on">
-      <span class="item_icon">
+    <span href="" class="footer_item" :class="{on:$route.path==='/home'}" @click="goto('/home')">
+      <span class="footer_icon">
         <i class="iconfont icon-shouye1"></i>
       </span>
       <span>首页</span>
     </span>
-    <span href="" class="guide_item on">
-      <span class="item_icon">
+    <span href="" class="footer_item " :class="{on:$route.path==='/search'}" @click="goto('/search')">
+      <span class="footer_icon">
         <i class="iconfont icon-icon-"></i>
       </span>
       <span>分类</span>
     </span>
-    <span href="" class="guide_item on">
-      <span class="item_icon">
+    <span href="" class="footer_item " :class="{on:$route.path==='/shop'}" @click="goto('/shop')">
+      <span class="footer_icon">
         <i class="iconfont icon-gouwuche"></i>
       </span>
       <span>购物车</span>
     </span>
-    <span href="" class="guide_item on">
-      <span class="item_icon">
+    <span href="" class="footer_item" :class="{on:$route.path==='/myec'}" @click="goto('/myec')">
+      <span class="footer_icon">
         <i class="iconfont icon-shouye"></i>
       </span>
       <span>我的E宠</span>
@@ -28,9 +28,46 @@
 </template>
 
 <script>
-  export default {}
+  export default {
+    methods:{
+      goto(path){
+        //跳转到指定路由 push/replace
+        this.$router.replace(path)
+        //console.log(this); //VueComponent{},$router:VueRouter
+      }
+    }
+  }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  .footer_guide
+    z-index 100
+    background-color #fff
+    height 45px
+    position fixed
+    bottom 0
+    left 0
+    right 0
+    width 100%
+    display flex
+    /*justify-content space-around*/
+    border-top 1px solid #e7e7e7
+    .footer_item
+      display flex
+      flex 1
+      text-align center
+      flex-direction column
+      align-items  center
+      margin 5px
+      color #999999
+      &.on
+        color #8FBC8F
+      span
+        font-size 12px
+        margin 1px 0
+        .iconfont
+          font-size 22px
+
+
 
 </style>
--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,13 +1,436 @@
 <template>
-  <div>
-    Home.vue
+  <div class="main">
+    <!--头部-->
+    <div class="header">
+      <!--头部-->
+      <div class="header_inner">
+        <div class="header_box">
+          <!--头部上半部分-->
+          <div class="header_top">
+            <div class="header_top_inner">
+              <div class="header_site">
+                <a href="">
+                  <span class="gougou">狗狗</span>
+                  <span class="c89">|</span>
+                  <span class="chongqing">重庆</span>
+                  <i class="iconfont icon-sanjiao"></i>
+                </a>
+              </div>
+              <div class="header_search">
+                <a href="">
+                  <input class="header_input" type="search" placeholder="搜索商品和品牌">
+                  <i class="iconfont icon-fangdajing"></i>
+                </a>
+              </div>
+              <a class="header_chat">
+                <i class="iconfont icon-liaotian1"></i>
+              </a>
+            </div>
+          </div>
+          <!--头部下半部分-->
+          <div class="header_bottom">
+            <div class="header_nav swiper-container">
+              <ul class="header_list swiper-wrapper">
+                <li class="header_item swiper-slide on">
+                  <a href="">
+                <span class="header_rela">
+                  <span>首页</span>
+                  <i class="iconfont icon-jianhao"></i>
+                </span>
+                  </a>
+                </li>
+                <li class="header_item swiper-slide">
+                  <a href="">
+                <span class="header_rela">
+                  <span>狗狗主粮</span>
+                  <i></i>
+                </span>
+                  </a>
+                </li>
+                <li class="header_item swiper-slide">
+                  <a href="">
+                <span class="header_rela">
+                  <span>服饰城</span>
+                  <i></i>
+                </span>
+                  </a>
+                </li>
+                <li class="header_item swiper-slide">
+                  <a href="">
+                <span class="header_rela">
+                  <span>医疗保健</span>
+                  <i></i>
+                </span>
+                  </a>
+                </li>
+                <li class="header_item swiper-slide">
+                  <a href="">
+                <span class="header_rela">
+                  <span>零食玩具</span>
+                  <i></i>
+                </span>
+                  </a>
+                </li>
+                <li class="header_item swiper-slide">
+                  <a href="">
+                <span class="header_rela">
+                  <span>日用外出</span>
+                  <i></i>
+                </span>
+                  </a>
+                </li>
+                <li class="header_item swiper-slide">
+                  <a href="">
+                <span class="header_rela">
+                  <span>美容香波</span>
+                  <i></i>
+                </span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--眨眼-->
+      <div class="header_blink">
+        <div class="header_mao"></div>
+      </div>
+    </div>
+    <!--内容区-->
+    <div class="content">
+      <!--轮播图-->
+      <div class="banner_scroll swiper-container">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">
+            <a href="">
+              <img src="../../../static/images/94d4681e2ab878c340d441a5c1c7b98a.jpg" alt="">
+            </a>
+          </div>
+          <div class="swiper-slide">
+            <a href="">
+              <img src="../../../static/images/9f49bbf054e4dcab567e1584f48db3f0.jpg" alt="">
+            </a>
+          </div>
+          <div class="swiper-slide">
+            <a href="">
+              <img src="../../../static/images/d945d3f1b34e0081cf8c9eb34def9096.jpg" alt="">
+            </a>
+          </div>
+          <div class="swiper-slide">
+            <a href="">
+              <img src="../../../static/images/f0e3af5548da9b04aa098004e07ff6c1.jpg" alt="">
+            </a>
+          </div>
+          <div class="swiper-slide">
+            <a href="">
+              <img src="../../../static/images/b898562338c7f04ac9b2fa3e5aec8b4c.jpg" alt="">
+            </a>
+          </div>
+        </div>
+        <div class="swiper-pagination"></div>
+      </div>
+      <!--列表-->
+      <div class="content_xqing">
+        <div>
+          <ul class="content_list">
+            <li class="content_item">
+              <a href="">
+                <img src="../../../static/images/4e820b334820dbb62178bd367cdd7ba6.jpg" alt="">
+              </a>
+            </li>
+            <li class="content_item">
+              <a href="">
+                <img src="../../../static/images/9d335309be26f610a043e3bcc1e84b31.jpg" alt="">
+              </a>
+            </li>
+            <li class="content_item">
+              <a href="">
+                <img src="../../../static/images/f1d45afa77b1844883595df4be0b6b50.jpg" alt="">
+              </a>
+            </li>
+            <li class="content_item">
+              <a href="">
+                <img src="../../../static/images/55231bc567328e7c57cde19809769d99.jpg" alt="">
+              </a>
+            </li>
+            <li class="content_item">
+              <a href="">
+                <img src="../../../static/images/59043067d26a2e7a521394c796dfa571.jpg" alt="">
+              </a>
+            </li>
+            <li class="content_item">
+              <a href="">
+                <img src="../../../static/images/8a3fa0fdcfe58afc3ab548f0cf8493ad.jpg" alt="">
+              </a>
+            </li>
+            <li class="content_item">
+              <a href="">
+                <img src="../../../static/images/bfb4d746361712b8899000ba21ae55dd.jpg" alt="">
+              </a>
+            </li>
+            <li class="content_item">
+              <a href="">
+                <img src="../../../static/images/08779496e825394821a9bb96e1ee2b29.jpg" alt="">
+              </a>
+            </li>
+            <li class="content_item">
+              <a href="">
+                <img src="../../../static/images/015feab01fe5c19def0f1b83bf140b70.jpg" alt="">
+              </a>
+            </li>
+            <li class="content_item">
+              <a href="">
+                <img src="../../../static/images/a561f8d8f46d4070de082988134de6f4.jpg" alt="">
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <!--新人-->
+      <div class="content_new">
+        <div class="swiper-container">
+          <div class="swiper-wrapper">
+            <div class="swiper-slide">
+              <a href="">
+                <img src="../../../static/images/60c354a5d94be9fd114523ee77259c73.gif" alt="">
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--每日疯抢-->
+      <div class="content_surprise">
+        <div class="surprise_top">
+          <div class="surprise_every">
+              <img src="../../../static/images/new_index_icon_suprice.png" alt="">
+          </div>
+          <div class="surprise_nexttip">下一场开始</div>
+          <div class="surprise_time">
+            <div class="time_xqing">
+              <span class="time_xqing1">16</span>
+              <span class="time_xqing2">:</span>
+              <span class="time_xqing1">00</span>
+            </div>
+          </div>
+          <div class="surprise_more">
+            <a href="">
+              <img src="../../../static/images/80acfffe2d91b341fd2c8de903b3eace.png" alt="">
+            </a>
+          </div>
+        </div>
+        <div class="surprise_bottom"></div>
+      </div>
+    </div>
+    <!--练习我们-->
+    <div class="about"></div>
   </div>
 </template>
 
 <script>
-  export default {}
+  import Swiper from 'swiper'
+  import 'swiper/dist/css/swiper.min.css'
+  export default {
+    mounted(){
+      /*头部导航*/
+      var headerswiper = new Swiper('.header_bottom .swiper-container', {
+        slidesPerView: 5,
+        spaceBetween: 5,
+        pagination: {
+          el: '.header_bottom .swiper-pagination',
+          clickable: true,
+        },
+      });
+
+      /*轮播图*/
+      var autoswiper = new Swiper('.content .swiper-container', {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+          delay: 4000,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: '.content .swiper-pagination',
+          clickable: true,
+        },
+      });
+
+      /*新人*/
+      var newswiper = new Swiper('.content_new .swiper-container');
+    }
+  }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "http://at.alicdn.com/t/font_595599_yasg6naao5tjfw29.css"
+  @import "../../common/stylus/mixins.styl"
+  @import "../../../static/css/reset.css"
 
+  .main
+    .header
+      z-index 100
+      width 100%
+      background-color #ffffff
+      position fixed
+      top 0
+      .header_inner
+        width 100%
+        height 86px
+        .header_box
+          .header_top
+            padding 10px 10px
+            .header_top_inner
+              background #fff
+              text-align center
+              width 100%
+              display flex
+              justify-content space-between
+              flex-wrap nowrap
+              .header_site
+                font-size 14px
+                margin-right 10px
+                color #898989
+                a>span
+                  vertical-align middle
+              .header_search
+                margin-right 5px
+                position relative
+                .header_input
+                  width 100%
+                  height 25px
+                  line-height 25px
+                  background #e9e9e9
+                  border 0px
+                  border-radius 4px
+                  color #bcbcbc
+                  font-size 13px
+                  outline 0
+                  padding-left 10px
+                .icon-fangdajing
+                  font-size 11px
+                  position absolute
+                  right 5px
+                  top 5px
+                  bottom 0px
+              .header_chat
+                display block
+                .icon-liaotian1
+                  font-size 20px
+                  margin-left 10px
+        .header_bottom
+          margin-bottom 10px
+          .header_nav
+            .header_list
+              .header_item
+                a
+                  display block
+                  height 100%
+                  width 100%
+                  line-height 36px
+                  font-size 14px
+                  text-align center
+                  color #666
+                  .header_rela
+                    position relative
+              li.on span
+                color #459d36
+                display inline-block
+              li.on i
+                position absolute
+                bottom -9px
+                left 5px
+                color #459d36
+
+
+    .content
+      margin-top 86px
+      .swiper-container //轮播
+        width 100%
+        height 100%
+        margin 0 auto
+        position relative
+        overflow hidden
+        .swiper-wrapper
+          .swiper-slide
+            text-align center
+            font-size 18px
+            a
+              display block
+              cursor pointer
+              img
+                width 100%
+                display block
+                vertical-align middle
+      .content_xqing //详情
+        .content_list
+          display flex
+          justify-content space-around
+          flex-wrap wrap
+          .content_item
+            width 20%
+            img
+              width 100%
+              display block
+      .content_new //新人
+        border 0
+        .swiper-container
+          width 100%
+          height 100%
+          margin 0 auto
+          position relative
+          overflow hidden
+          .swiper-wrapper
+            .swiper-slide
+              text-align center
+              font-size 18px
+              a
+                display block
+                cursor pointer
+                img
+                  width 100%
+                  display block
+                  vertical-align middle
+      .content_surprise //每日疯抢
+        background-color #fff
+        .surprise_top
+          position relative
+          padding 10px
+          .surprise_every
+            float left
+            img
+              width 85px
+              display block
+              vertical-align middle
+
+          .surprise_nexttip
+            position absolute
+            right 107px
+            margin-right 5px
+          .surprise_time
+            position absolute
+            right 55px
+            .time_xqing
+              display inline-block
+              font-size 12px
+              .time_xqing1
+                border 1px solid #ddd
+                padding 1px 2px
+                font-size 13px
+              .time_xqing2
+                margin 0 3px
+                font-weight 700
+          .surprise_more
+            position absolute
+            top 0px
+            right -8px
+            a
+              display block
+              text-align right
+              img
+                vertical-align middle
+                border 0
+                width 85px
+                margin 0 0 0 auto
+                display block
 </style>
